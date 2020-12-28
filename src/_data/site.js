e89b9c1d672aaba70a0750422aab3665518b6212
helloworldwriter <writer@helloworldwriter.com>
2020-12-28 18:48:09 +0200

    Add section menu

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ const isProduction = process.env.ELEVENTY_ENV == 'production';
 
 module.exports = {
   url: isProduction ? 'https://www.aurelitec.com' : 'http://localhost:8080',
-  imagesUrl: 'https://static.aurelitec.com/images',
+  imagesUrl: 'https://ik.imagekit.io/aurelitec',
   // downloadsBaseUrl: 'https://download.aurelitec.com',
   buildYear: new Date().getFullYear(),
   isProduction,
